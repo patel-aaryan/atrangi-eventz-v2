fa5b8f5c9aed4ab518949654fa42b94946e02fa5
Aaryan Patel <a52patel@uwaterloo.ca>
2026-02-01 02:59:03 -0500

    updates

--- packages/server/infra/src/qstash.ts
+++ packages/server/infra/src/qstash.ts
@@ -1,7 +1,10 @@
 import { Client } from "@upstash/qstash";
 
 const qstashToken = process.env.QSTASH_TOKEN;
+const qstashUrl = process.env.QSTASH_URL;
 
 if (!qstashToken) throw new Error("QSTASH_TOKEN is not set");
 
-export const qstash = new Client({ token: qstashToken });
+if (!qstashUrl) throw new Error("QSTASH_URL is not set");
+
+export const qstash = new Client({ baseUrl: qstashUrl, token: qstashToken });
